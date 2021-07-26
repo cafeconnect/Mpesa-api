d916ebad183b2fc1477416fe387080dd18be92f9
mathew <mathewmoroko@gmail.com>
2021-07-26 10:23:45 +0300

    first commit

---
+++ app/Http/Controllers/MpesaController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\MpesaTransaction;
+
+class MpesaController extends Controller
+{
+  
+    public function lipaNaMpesaPassword()
+    {
+        $lipa_time = Carbon::rawParse('now')->format('YmdHms');
+        $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+        $BusinessShortCode = 174379;
+        $timestamp =$lipa_time;
+        $lipa_na_mpesa_password = base64_encode($BusinessShortCode.$passkey.$timestamp);
+        return $lipa_na_mpesa_password;
+    }
+  
+    
+   public function generateAccessToken()
+    {
+        $consumer_key="a1bUCFngqjwGbF2nu6zJMZGkFEkp4JOP";
+        $consumer_secret="oR9Qks7tJjLBalDu";
+        $credentials = base64_encode($consumer_key.":".$consumer_secret);
+        $url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Basic ".$credentials));
+        curl_setopt($curl, CURLOPT_HEADER,false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl_response = curl_exec($curl);
+        $access_token=json_decode($curl_response);
+        return $access_token->access_token;
+    }
+    public function customerMpesaSTKPush(Request $request)
+    {
+          $amount = $request->amount;
+          $location = $request->location;
+          $name = $request->name;
+          $phone = $request->mobile;
+          $formatedPhone = substr($phone, 1);
+          $code = "254";
+          $phoneNumber = $code.$formatedPhone;
+
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generateAccessToken()));
+        $curl_post_data = [
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => 174379,
+            'Password' => $this->lipaNaMpesaPassword(),
+            'Timestamp' => Carbon::rawParse('now')->format('YmdHms'),
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $amount,
+            'PartyA' => $phoneNumber,
+             'PartyB' => 174379,
+            'PhoneNumber' => $phoneNumber, 
+            'CallBackURL' => 'https://f6707d1d0da8.ngrok.io/api/v1/hlab/transaction/confirmation',
+            'AccountReference' => "moroko investment",
+            'TransactionDesc' => "Testing stk push on sandbox"
+        ];
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        return $curl_response;
+    }
+    public function mpesaConfirmation(Request $request)
+    {
+        $content=json_decode($request->getContent());
+        $mpesa_transaction = new MpesaTransaction();
+        $mpesa_transaction->response = json_encode($content);
+        $mpesa_transaction->save();   
+  }
+}
